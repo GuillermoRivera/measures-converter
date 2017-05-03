c08f6ddb94ad27f83bd0b49a31faa2009a9645b7
Juan Guillermo Rivera <guillerivera32@gmail.com>
2017-05-03 08:59:33 -0500

    Added no negativity condition to the measure

--- dist/src/converter.js
+++ dist/src/converter.js
@@ -12,12 +12,16 @@ function converter(w, str) {
   var kilos = 0.453592;
 
   if (typeof weight === 'number') {
-    if (weightType === 'kilograms') {
-      convertedWeight = weight * pounds;
-    } else if (weightType === 'pounds') {
-      convertedWeight = weight * kilos;
+    if (weight >= 0) {
+      if (weightType === 'kilograms' || weightType === 'kilos') {
+        convertedWeight = weight * pounds;
+      } else if (weightType === 'pounds') {
+        convertedWeight = weight * kilos;
+      } else {
+        return console.log('You have to enter a valid measure:\n1. Kilograms\n2. Pounds');
+      }
     } else {
-      return console.log('You have to enter a valid measure:\n1. Kilograms\n2. Pounds');
+      return console.log('You can not enter a negative value');
     }
   } else {
     return console.log('Sorry, the measure must be a number');
