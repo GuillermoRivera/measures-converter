1126858686f3ca12faeab0569d1ca5bc688d2b62
Juan Guillermo Rivera <guillerivera32@gmail.com>
2017-05-03 08:47:24 -0500

    Added babel to compile the src/converter.js to dist/src/converter.js for compatibility with all browsers

---
+++ dist/src/converter.js
@@ -0,0 +1,27 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = converter;
+function converter(w, str) {
+  var weight = w;
+  var weightType = str.toLowerCase();
+  var convertedWeight = 0;
+  var pounds = 2.20462;
+  var kilos = 0.453592;
+
+  if (typeof weight === 'number') {
+    if (weightType === 'kilograms') {
+      convertedWeight = weight * pounds;
+    } else if (weightType === 'pounds') {
+      convertedWeight = weight * kilos;
+    } else {
+      return console.log('You have to enter a valid measure:\n1. Kilograms\n2. Pounds');
+    }
+  } else {
+    return console.log('Sorry, the measure must be a number');
+  }
+
+  return parseFloat(convertedWeight.toFixed(5));
+}
